f5d8b0cc106516a4b98996aa188d48d4c11e5349
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-05-02 15:32:47 -0700

    added env variables

--- pb-p-site/src/payload.config.ts
+++ pb-p-site/src/payload.config.ts
@@ -66,8 +66,27 @@ export default buildConfig({
   cors: [getServerSideURL()].filter(Boolean),
   globals: [Header, Footer],
   plugins: [
-    ...plugins,
-    // storage-adapter-placeholder
+      s3Storage({
+        collections: {
+          media: true,
+          'media-with-prefix': {
+            prefix,
+          },
+        },
+        bucket: process.env.S3_BUCKET,
+        config: {
+          credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+          },
+          region: process.env.S3_REGION,
+          endpoint: process.env.S3_ENDPOINT,
+          forcePathStyle: true,
+          // ... Other S3 configuration
+        },
+      }),
+    ],
+  })
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
